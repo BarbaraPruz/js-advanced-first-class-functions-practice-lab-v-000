cae8737088ff9b2de834d8d3d6fd9423c8cfc8d9
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-28 22:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach( driver => console.log(driver.name));
+}
+
+function logDriversByHometown(drivers, town) {
+  drivers.forEach( function (driver) {
+    if (driver.hometown === town) console.log(driver.name);
+  });
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort( (a,b) => a.revenue - b.revenue );
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort( (a,b) => a.name.localeCompare(b.name));
+}
